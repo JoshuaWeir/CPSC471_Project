745bbd01330ec1cf6dc7f761683f658d02f6ea13
ErnestSarna <ernest.sarna@ucalgary.ca>
2023-04-08 12:00:19 -0600

    added files

---
+++ Author.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Author {
+    private String name;
+    private Date birthDate;
+    private String countryOfOrigin;
+    private String shortBib;
+    private int numOfBooks;
+}
